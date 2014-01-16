417fe89b4ad83a2b39d7482d9934f9488d032309
zeMirco <mirco.zeiss@gmail.com>
2014-01-16 17:16:20 +0100

    add karma conf and specs

---
+++ test/unit/services/undo.spec.js
@@ -0,0 +1,12 @@
+
+describe('services', function() {
+  
+  describe('undo', function() {
+    
+    it('should return current version', function(notification) {
+      expect('0.1').toEqual('0.1');
+    });
+    
+  });
+  
+});
